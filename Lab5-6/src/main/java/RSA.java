d3df9d1479e57f784b535239744f3db0c38bbbe7
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2025-04-03 23:08:18 +0300

    Update RSA.java

--- Lab5-6/src/main/java/RSA.java
+++ Lab5-6/src/main/java/RSA.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 public class RSA {
     // зміна за варіантом + обране е
     public static int p = 23;
@@ -61,32 +63,11 @@ public class RSA {
         return d;
     }
 
-    // розширений алгоритм Евкліда
-    public static int modInverse(int a, int m) {
-        int m0 = m;
-        int y = 0, x = 1;
-
-        if (m == 1) {
-            return 0;
-        }
-
-        while (a > 1) {
-            int q = a / m;
-            int t = m;
-
-            m = a % m;
-            a = t;
-
-            t = y;
-            y = x - q * y;
-            x = t;
-        }
-
-        if (x < 0) {
-            x += m0;
-        }
-
-        return x;
+    // e ^ (-1)mod ф(n) за допомогою BigInteger
+    public static int modInverse(int e, int m) {
+        BigInteger A = BigInteger.valueOf(e);
+        BigInteger M = BigInteger.valueOf(m);
+        return A.modInverse(M).intValue();
     }
 
     // розшифрування тексту
